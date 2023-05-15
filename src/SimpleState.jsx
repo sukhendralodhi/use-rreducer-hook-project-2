97b3c1211bb23ca2d11b0058e8fb646dc825c093
Sukhendra Lodhi <mpct5jan1998@gmail.com>
2023-05-15 14:31:19 +0530

    Final done

--- src/SimpleState.jsx
+++ src/SimpleState.jsx
@@ -1,39 +1,45 @@
 import { useState } from "react";
 import { data } from "./data";
 
-
 const SimpleState = () => {
-    const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState(data);
 
-    const handleRemove = (id) => {
-        const newPeople = people.filter((person) => person.id !== id);
-        setPeople(newPeople);
-    }
+  const handleRemove = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
 
-    const handleRemoveAll = () => {
-        setPeople([]);
-    }
+  const handleRemoveAll = () => {
+    setPeople([]);
+  };
 
-    const handleResetAll = () => {
-        setPeople(data);
-    }
-    return (
-        <div>
-            <h3>Using useState</h3>
-            {people.map((person) => {
-                const { id, name } = person;
-                return (
-                    <div key={id}>
-                        <h4>{name}</h4>
-                        <button onClick={() => handleRemove(id)}>remove</button>
-                    </div>
-                );
-            })}
+  const handleResetAll = () => {
+    setPeople(data);
+  };
+  return (
+    <div>
+      <h3>Using useState</h3>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button onClick={() => handleRemove(id)}>remove</button>
+          </div>
+        );
+      })}
 
-            {people.length > 0 ? (<button onClick={() => handleRemoveAll()} className="btn">clear all</button>) : (<button onClick={() => handleResetAll()} className="btn">reset all</button>)}
-
-        </div>
-    );
-}
+      {people.length > 0 ? (
+        <button onClick={() => handleRemoveAll()} className="btn">
+          clear all
+        </button>
+      ) : (
+        <button onClick={() => handleResetAll()} className="btn">
+          reset all
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default SimpleState;
